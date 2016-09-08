67f1eed82f233dd619ec0828f95954c8c416066c
Taylor Denouden <taylor.denouden@hakai.org>
2016-09-08 14:56:07 -0700

    Bump to v0.1.0 and update readme to reflect stable working package.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='seabirdSBE',
     packages=['seabirdSBE'],
-    version='0.0.3',
+    version='0.1.0',
     description='An abstraction of SBE command line exec utils for python',
     author='Taylor Denouden',
     author_email='taylordenouden@gmail.com',
     url='https://github.com/HakaiInstitute/seabirdSBE',
-    download_url='https://github.com/HakaiInstitute/seabirdSBE/tarball/0.0.3',
+    download_url='https://github.com/HakaiInstitute/seabirdSBE/tarball/0.1.0',
     keywords=['seabird', 'ctd', 'sbe', 'hakai'],
     classifiers=[],
 )
